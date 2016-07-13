5048ccb24cd6b273385f8e518a49cda540d77934
hongzhang <hongzhang@papatv.cn>
2016-07-13 13:39:59 +0800

    add zookeeper demo and gradle demo

---
+++ zookeeper/src/main/java/com/hz/demo/Demo.java
@@ -0,0 +1,10 @@
+package com.hz.demo;
+
+/**
+ * Created by hz on 2016/7/12.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello zk !");
+    }
+}
